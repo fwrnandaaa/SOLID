69948475e4dde729ee37781a039eb107471709c8
Fernanda <110307073+fwrnandaaa@users.noreply.github.com>
2023-10-23 12:30:21 -0300

    Solução princípio da segregação de interface

---
+++ atividades/segregacao_interface/Solução/AllInOnePrinter.java
@@ -0,0 +1,5 @@
+public class AllInOnePrinter implements Machine {
+    @Override
+    public void print() {
+        // Como o nome da classe da a entender que ela só irá utilizar o método print, foi o que eu deixei aqui.
+    }
